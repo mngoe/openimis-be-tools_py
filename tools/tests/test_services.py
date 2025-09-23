fd97e7dd95b92b5728517e1481da1d3149b35c76
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-09-23 12:44:07 +0100

    Update test_services.py

--- tools/tests/test_services.py
+++ tools/tests/test_services.py
@@ -3,6 +3,7 @@ from tablib import Dataset
 from core.test_helpers import create_test_interactive_user
 from django.test import TestCase
 from tools.resources import ServiceResource
+import importlib.resources
 
 
 class ImportServiceTest(TestCase):
@@ -11,13 +12,11 @@ class ImportServiceTest(TestCase):
         super(ImportServiceTest, self).setUp()
         self.user = create_test_interactive_user()
 
-
     def test_simple_import(self):
-        dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         resource = ServiceResource(self.user.id_for_audit)
         dataset = Dataset()
 
-        with open(os.path.join(dir_path, 'tests/service_example.json'), 'r') as f:
+        with importlib.resources.open_text("tools.tests", "service_example.json") as f:
             dataset.load(f.read())
             result = resource.import_data(
                 dataset, dry_run=True, use_transactions=True,
@@ -25,8 +24,6 @@ class ImportServiceTest(TestCase):
             )
             self.assertEqual(result.has_errors(), False)
 
-
     def test_simple_export(self):
-        result = ServiceResource(self.user).export().dict
+        result = ServiceResource(self.user.id_for_audit).export().dict
         self.assertTrue(result)
-        
