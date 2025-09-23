a74247b063d0bb1861eed537280a04165a969f6a
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-09-23 13:08:48 +0100

    Fix unit tests add update manifest

--- tools/tests/test_services.py
+++ tools/tests/test_services.py
@@ -3,7 +3,6 @@ from tablib import Dataset
 from core.test_helpers import create_test_interactive_user
 from django.test import TestCase
 from tools.resources import ServiceResource
-import importlib.resources
 
 
 class ImportServiceTest(TestCase):
@@ -13,10 +12,11 @@ class ImportServiceTest(TestCase):
         self.user = create_test_interactive_user()
 
     def test_simple_import(self):
+        dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         resource = ServiceResource(self.user.id_for_audit)
         dataset = Dataset()
 
-        with importlib.resources.open_text("tools.tests", "service_example.json") as f:
+        with open(os.path.join(dir_path, 'tests/service_example.json'), 'r') as f:
             dataset.load(f.read())
             result = resource.import_data(
                 dataset, dry_run=True, use_transactions=True,
