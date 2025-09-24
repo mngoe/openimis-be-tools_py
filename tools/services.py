f5dc21d6f0abed8d1d673c327b624c32f8c2f99b
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-09-24 15:51:30 +0100

    feat: adding alias

--- tools/services.py
+++ tools/services.py
@@ -969,7 +969,7 @@ def create_master_data_export(user):
             "GeneralAssemblyLumpSum", "GeneralAssemblyFee", "StartCycle1", "StartCycle2", "StartCycle3",
             "StartCycle4", "GracePeriodRenewal", "MaxInstallments", "WaitingPeriod", "Threshold",
             "RenewalDiscountPerc", "RenewalDiscountPeriod", "AdministrationPeriod", "EnrolmentDiscountPerc",
-            "EnrolmentDiscountPeriod", "GracePeriod", "Program", "Min Age", "Max Age"
+            "EnrolmentDiscountPeriod", "GracePeriod", "Program", "Min Age" AS minAge, "Max Age" AS maxAge
         FROM "tblProduct" WHERE "ValidityTo" IS NULL"""
         if connection.vendor == "postgresql" else
         """SELECT "ProdID", "ProductCode", "ProductName", "LocationId", "InsurancePeriod",
@@ -978,7 +978,7 @@ def create_master_data_export(user):
             "GeneralAssemblyLumpSum", "GeneralAssemblyFee", "StartCycle1", "StartCycle2", "StartCycle3",
             "StartCycle4", "GracePeriodRenewal", "MaxInstallments", "WaitingPeriod", "Threshold",
             "RenewalDiscountPerc", "RenewalDiscountPeriod", "AdministrationPeriod", "EnrolmentDiscountPerc",
-            "EnrolmentDiscountPeriod", "GracePeriod", "Program", "Min Age", "Max Age"
+            "EnrolmentDiscountPeriod", "GracePeriod", "Program", "Min Age" AS minAge, "Max Age" AS maxAge
         FROM "tblProduct" WHERE "ValidityTo" IS NULL""",
         "professions": """SELECT "ProfessionId", "Profession", "SortOrder", "AltLanguage" FROM "tblProfessions";""",
         "relations": """SELECT "RelationId", "Relation", "SortOrder", "AltLanguage" FROM "tblRelations";""",
